6c9c52f3561b88da4693c6986afc6cb01f83bc7e
n8nny <31177297+N8NNY@users.noreply.github.com>
2023-01-15 14:58:40 +0700

    :tada: init code

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var webpack = require('webpack');
+var path = require('path');
+var GlobEntries = require('webpack-glob-entries');
+
+module.exports = {
+  mode: 'production',
+  entry: GlobEntries('./src/*test*.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs',
+    filename: '[name].bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+      },
+    ],
+  },
+  stats: {
+    colors: true,
+  },
+  target: 'web',
+  externals: /^(k6|https?\:\/\/)(\/.*)?/,
+  devtool: 'source-map',
+};
